c9564d4194bc88f6fb06753a8280f36ee6124c3a
LiveTheFuture <81014027+LiveTheFuture@users.noreply.github.com>
2021-05-12 12:53:08 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { request } = require('express');
 const express = require ('express');
 const app = express();
-const port = 4000;
+const PORT= process.env.PORT || 4000;
 
 //SET UP MONGOOSE
 const mongoose = require ('mongoose');
@@ -91,7 +91,7 @@ app.delete('/intern/:id', (req, res) => {
 })
 
 
-app.listen(port, () => {
-    console.log(`app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`)
 });
 
